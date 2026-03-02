3f1c701592ad1e5ae0a32de810a6472b527096f6
ggurung1 <gurungmsc2014@gmail.com>
2026-03-02 16:15:28 +0000

    updated GitHub Pages

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+const params=new URLSearchParams(window.location.search);
+
+const project =params.get("project")
+const title=document.getElementById("dashboard-title")
+const gallery=document.getElementById("dashboard-gallery")
 const toggle = document.getElementById("theme-toggle");
 
 /* Load saved theme */
@@ -16,3 +21,53 @@ toggle.addEventListener("click", () => {
   updateButtonText();
 });
 
+
+if (project=== "project1") {
+  title.textContent= "E-commerce Sales Dashboards";
+
+  gallery.innerHTML=`
+  <figure>
+  <img src="assets/images/project1/Sales.png" alt="Sales KPI Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  <figure>
+  <img src="assets/images/project1/Category.png" alt="Category Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  <figure>
+  <img src="assets/images/project1/Customer.png" alt="Customer Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  <figure>
+  <img src="assets/images/project1/Return.png" alt="Return Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  `;
+}
+
+if (project=== "project2") {
+  title.textContent= "Project 2";
+
+  gallery.innerHTML=`
+  <figure>
+  <img src="assets/images/project1/Sales.png" alt="Sales KPI Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  <figure>
+  <img src="assets/images/project1/Category.png" alt="Sales KPI Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  `;
+}
+
+
+if (project=== "project3") {
+  title.textContent= "Project3";
+
+  gallery.innerHTML=`
+  <figure>
+  <img src="assets/images/project1/Sales.png" alt="Sales KPI Dashboard">
+  <figcaption> Caption </figcaption>
+  </figure>
+  `;
+}
